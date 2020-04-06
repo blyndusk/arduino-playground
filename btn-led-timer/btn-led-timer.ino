c1d9b10f8637ce4372b6c9343a97c80e3affd12a
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-04-06 19:34:08 +0200

    :boom: btn red timer fully functionnal

--- btn-led-timer/btn-led-timer.ino
+++ btn-led-timer/btn-led-timer.ino
@@ -17,38 +17,53 @@ int currentState;
 int ledState = LOW;
 bool isTimerReseted = true;
 
+const setLowAndBlinkSpecificLed(int currentLed) {
+  if (currentLed  < LEDS_AMOUNT) {
+    digitalWrite(TIMER_LED_PINS[currentLed + 1], LOW);
+  }
+  digitalWrite(TIMER_LED_PINS[currentLed], LOW);
+  delay(500);
+  digitalWrite(TIMER_LED_PINS[currentLed], HIGH);
+  delay(500);
+  Serial.print("Remaining time: ");
+  Serial.println(seconds);
+
+}
+
 const switchOffLed(int currentLed) {
   if (
     seconds <  TIMER_PORTION * currentLed + 1 &&
     seconds >= TIMER_PORTION * currentLed
-  ) {
-    digitalWrite(TIMER_LED_PINS[currentLed], LOW);
+  ) {;
+    for (int j = 0; j < TIMER_PORTION; j++) {
+      setLowAndBlinkSpecificLed(currentLed);
+    }
   }
 }
 
 const startAndEndTimer() {
   Serial.println("TIMER:[START]");
+  digitalWrite(YELLOW_PIN, LOW);
   if (seconds > 0) {
     for (int i = 0; i < TIMER; i++) {
       seconds--;
-      Serial.print("Remaining time: ");
-      Serial.println(seconds);
       for (int i = 0; i < TIMER; i++) {
         const int LED_INDEX = TIMER - i - 1;
         switchOffLed(TIMER - i - 1);
       }
       if (seconds == 0) {
-        Serial.println("TIMER:[END]");
+        setLowAndBlinkSpecificLed(0);
+        digitalWrite(TIMER_LED_PINS[0], LOW);
         digitalWrite(YELLOW_PIN, HIGH);
+        Serial.println("TIMER:[END]");
       }
-      delay(1000);
     }
   }
 }
 
 const resetTimer() {
   seconds = TIMER;
-  digitalWrite(YELLOW_PIN, LOW);
+  digitalWrite(YELLOW_PIN, HIGH);
   for (int i = 0; i < TIMER; i++) {
     digitalWrite(TIMER_LED_PINS[i], HIGH);
   }
@@ -59,7 +74,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(BTN_PIN, INPUT_PULLUP);
   pinMode(YELLOW_PIN, OUTPUT);
-  digitalWrite(YELLOW_PIN, LOW);
+  digitalWrite(YELLOW_PIN, HIGH);
   for (int i = 0; i < LEDS_AMOUNT; i++) {
     pinMode(TIMER_LED_PINS[i], OUTPUT);
     digitalWrite(TIMER_LED_PINS[i], HIGH);
