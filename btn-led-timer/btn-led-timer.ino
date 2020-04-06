f57ff6a89c36f78aac6840b52652da310d2ccf4c
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-04-06 18:42:21 +0200

    :bug: fix timer

--- btn-led-timer/btn-led-timer.ino
+++ btn-led-timer/btn-led-timer.ino
@@ -6,40 +6,37 @@ const int TIMER_LED_PINS[] = {
 const int YELLOW_PIN = 3;
 const int BTN_PIN = 2;
 
-const int TIMER = 9;
+const int TIMER = 27;
 const int LEDS_AMOUNT = 9;
 const int TIMER_PORTION = TIMER / LEDS_AMOUNT;
 
 int seconds = TIMER;
 
-const setLowAndBlinkSpecificLed(int currentLed) {
-  if (currentLed < LEDS_AMOUNT) {
-    digitalWrite(TIMER_LED_PINS[currentLed], LOW);
-  }
-  digitalWrite(TIMER_LED_PINS[currentLed - 1], LOW);
-  delay(500);
-  digitalWrite(TIMER_LED_PINS[currentLed - 1], HIGH);
-  delay(500);
-}
-
-
-// variables will change:
-int ledState = LOW;   // the current state of LED
-
+int lastState = HIGH;
+int currentState;
+int ledState = LOW;
 bool isTimerReseted = true;
 
-int lastState = HIGH; // the previous state from the input pin
-int currentState;    // the current reading from the input pin
+const switchOffLed(int currentLed) {
+  if (
+    seconds <  TIMER_PORTION * currentLed + 1 &&
+    seconds >= TIMER_PORTION * currentLed
+  ) {
+    digitalWrite(TIMER_LED_PINS[currentLed], LOW);
+  }
+}
 
 const startAndEndTimer() {
   Serial.println("TIMER:[START]");
   if (seconds > 0) {
-    for (int i = 0; i < LEDS_AMOUNT; i++) {
-      const int LED_INDEX = TIMER - i - 1;
+    for (int i = 0; i < TIMER; i++) {
       seconds--;
       Serial.print("Remaining time: ");
       Serial.println(seconds);
-      digitalWrite(TIMER_LED_PINS[LED_INDEX], LOW);
+      for (int i = 0; i < TIMER; i++) {
+        const int LED_INDEX = TIMER - i - 1;
+        switchOffLed(TIMER - i - 1);
+      }
       if (seconds == 0) {
         Serial.println("TIMER:[END]");
         digitalWrite(YELLOW_PIN, HIGH);
@@ -52,7 +49,7 @@ const startAndEndTimer() {
 const resetTimer() {
   seconds = TIMER;
   digitalWrite(YELLOW_PIN, LOW);
-  for (int i = 0; i < LEDS_AMOUNT; i++) {
+  for (int i = 0; i < TIMER; i++) {
     digitalWrite(TIMER_LED_PINS[i], HIGH);
   }
   Serial.println("TIMER:[RESET]");
